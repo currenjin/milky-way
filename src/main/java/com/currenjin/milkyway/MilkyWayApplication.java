fa0313fe7302b9eb90621db227fd89fbf1d0d767
nowjin <hyun0524e@naver.com>
2022-08-25 11:01:30 +0900

    add: initialize project

---
+++ src/main/java/com/currenjin/milkyway/MilkyWayApplication.java
@@ -0,0 +1,13 @@
+package com.currenjin.milkyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkyWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkyWayApplication.class, args);
+    }
+
+}
